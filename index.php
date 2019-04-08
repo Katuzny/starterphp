950519a02ff2fdcfa9df50fba2c5ce84aa1dc133
mastergoster <dugrais.julien@icloud.com>
2019-04-08 16:48:25 +0200

    defi formulaire

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 	<a href="/starterphp/boucle.php">les boucles</a>
 	<a href="/starterphp/condition.php">les conditions</a>
 	<a href="/starterphp/get.php">les variable dans les urls</a>
+	<a href="/starterphp/form.php">les formulaires</a>
 </body>
 </html>
